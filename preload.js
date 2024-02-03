443af36217098c0f5abf9afb335279c0a9cb2f7c
Zeng1998 <1129142694@qq.com>
2024-02-03 20:21:00 +0800

    feat: publish a new version
    - use access token instead of openid;
    - update api route;
    - can post memos directly;

--- preload.js
+++ preload.js
@@ -1,58 +1,137 @@
 const fs = require("fs");
 
-function convertBase64UrlToBlob(dataUrl) {
-    const bytes = window.atob(dataUrl.split(',')[1]);
-    const ab = new Uint8Array(bytes.length);
-    for (let i = 0; i < bytes.length; i++) {
-        ab[i] = bytes.charCodeAt(i);
+function appendContent(action, showAppendAlert, onSuccess, onError) {
+    const {type, payload} = action;
+    if (type === "over") {
+        try {
+            let content = utools.dbStorage.getItem("memos_content");
+            if (content) {
+                content += "\n" + payload;
+            } else {
+                content = payload;
+            }
+            utools.dbStorage.setItem("memos_content", content);
+            if (showAppendAlert) {
+                alert("添加文本成功!");
+            }
+            onSuccess();
+        } catch (err) {
+            alert(`添加文本失败! message: ${err}`);
+            onError();
+        }
+    } else if (type === "files") {
+        let resourceIdList = utools.dbStorage.getItem("memos_resource_id_list");
+        if (!resourceIdList) {
+            resourceIdList = [];
+        }
+        const token = utools.dbStorage.getItem("memos_access_token");
+        const url = utools.dbStorage.getItem("memos_url");
+        if (token && url) {
+            const fetches = payload.filter((file) => file.isFile).map((file) => {
+                const form = new FormData();
+                const data = fs.readFileSync(file.path);
+                form.append('file', new Blob([data]), file.name);
+                return fetch(`${url}/api/v1/resource/blob`, {
+                    method: 'POST',
+                    headers: {
+                        "Authorization": `Bearer ${token}`
+                    },
+                    body: form,
+                });
+            });
+            Promise.all(fetches)
+                .then(responses => {
+                    return Promise.all(responses.map(function (response) {
+                        return response.json();
+                    }));
+                })
+                .then(data => {
+                    Promise.all(data.map(function (dt) {
+                        if (dt) {
+                            resourceIdList.push(dt.id)
+                        } else {
+                            alert(`上传失败! error: ${JSON.stringify(dt)}`);
+                            onError();
+                        }
+                    }))
+                        .then(() => {
+                            try {
+                                utools.dbStorage.setItem("memos_resource_id_list", resourceIdList);
+                                alert(`添加资源成功! resourceIdList: ${JSON.stringify(resourceIdList)}`);
+                                onSuccess();
+                            } catch (err) {
+                                alert(`添加资源失败! message: ${err}`);
+                                onError();
+                            }
+                        });
+
+                })
+                .catch(err => {
+                    alert(`上传失败! error: ${err}`);
+                    onError();
+                });
+
+        } else {
+            alert("内部错误!");
+            onError();
+        }
+    } else {
+        alert("内部错误!");
+        onError();
     }
-    return new Blob([ab], {type: "image/png"});
+}
+
+function clearContent() {
+    utools.dbStorage.removeItem("memos_content");
+    utools.dbStorage.removeItem("memos_resource_id_list");
 }
 
 window.exports = {
     "post": {
         mode: "none",
         args: {
-            enter: () => {
+            enter: (action) => {
                 window.utools.hideMainWindow()
-                const openid = utools.dbStorage.getItem("memos_openid");
-                const url = utools.dbStorage.getItem("memos_url");
-                const content = utools.dbStorage.getItem("memos_content");
-                let resourceIdList = utools.dbStorage.getItem("memos_resource_id_list");
-                if (!resourceIdList) {
-                    resourceIdList = [];
-                }
-                if (openid && url) {
-                    fetch(`${url}/api/memo?openId=${openid}`, {
-                        method: 'POST',
-                        headers: {
-                            "Content-Type": " application/json",
-                        },
-                        body: JSON.stringify({
-                            "content": content,
-                            "resourceIdList": resourceIdList,
-                        }),
-                    })
-                        .then(response => response.json())
-                        .then(data => {
-                            if (data.data) {
-                                alert("发布成功!");
-                                utools.dbStorage.removeItem("memos_content");
-                                utools.dbStorage.removeItem("memos_resource_id_list");
-                                window.utools.outPlugin();
-                            } else {
-                                alert(`发布失败! error: ${JSON.stringify(data)}`);
-                                window.utools.outPlugin();
-                            }
+                appendContent(action, false, () => {
+                    const token = utools.dbStorage.getItem("memos_access_token");
+                    const url = utools.dbStorage.getItem("memos_url");
+                    const content = utools.dbStorage.getItem("memos_content");
+                    let resourceIdList = utools.dbStorage.getItem("memos_resource_id_list");
+                    if (!resourceIdList) {
+                        resourceIdList = [];
+                    }
+                    if (token && url) {
+                        fetch(`${url}/api/v1/memo`, {
+                            method: 'POST',
+                            headers: {
+                                "Content-Type": " application/json",
+                                "Authorization": `Bearer ${token}`
+                            },
+                            body: JSON.stringify({
+                                "content": content,
+                                "resourceIdList": resourceIdList,
+                            }),
                         })
-                        .catch(err => {
-                            alert(`发布失败! error: ${err}`);
-                            window.utools.outPlugin();
-                        })
-                } else {
-                    alert("请先设置 OpenID! (复制 OpenAPI 并打开 utools)");
-                    window.utools.outPlugin();
-                }
+                            .then(response => response.json())
+                            .then(data => {
+                                if (data) {
+                                    alert("发布成功!");
+                                    clearContent();
+                                    window.utools.outPlugin();
+                                } else {
+                                    alert(`发布失败! error: ${data}`);
+                                    window.utools.outPlugin();
+                                }
+                            })
+                            .catch(err => {
+                                alert(`发布失败! error: ${err}`);
+                                window.utools.outPlugin();
+                            })
+                    } else {
+                        alert("请先设置 Access Token! (复制 Access Token 并打开 utools)");
+                        window.utools.outPlugin();
+                    }
+                }, window.utools.outPlugin);
             },
         }
     },
@@ -61,120 +140,7 @@ window.exports = {
         args: {
             enter: (action) => {
                 window.utools.hideMainWindow()
-                const {type, payload} = action;
-                if (type === "over") {
-                    try {
-                        let content = utools.dbStorage.getItem("memos_content");
-                        if (content) {
-                            content += "\n" + payload;
-                        } else {
-                            content = payload;
-                        }
-                        utools.dbStorage.setItem("memos_content", content);
-                        alert("添加文本成功!");
-                        window.utools.outPlugin();
-                    } catch (err) {
-                        alert(`添加文本失败! message: ${err}`);
-                        window.utools.outPlugin();
-                    }
-                } else if (type === "img") {
-                    let resourceIdList = utools.dbStorage.getItem("memos_resource_id_list");
-                    if (!resourceIdList) {
-                        resourceIdList = [];
-                    }
-                    const openid = utools.dbStorage.getItem("memos_openid");
-                    const url = utools.dbStorage.getItem("memos_url");
-                    if (openid && url) {
-                        const file = convertBase64UrlToBlob(payload);
-                        const form = new FormData();
-                        form.append('file', file, `image.${payload.split(";")[0].slice(11)}`);
-                        fetch(`${url}/api/resource?openId=${openid}`, {
-                            method: 'POST',
-                            body: form,
-                        })
-                            .then(resp => resp.json())
-                            .then(data => {
-                                if (data.data) {
-                                    resourceIdList.push(data.data.id)
-                                } else {
-                                    alert(`上传失败! error: ${JSON.stringify(data)}`);
-                                    window.utools.outPlugin();
-                                }
-                            })
-                            .then(() => {
-                                try {
-                                    utools.dbStorage.setItem("memos_resource_id_list", resourceIdList);
-                                    alert(`添加资源成功! resourceIdList: ${JSON.stringify(resourceIdList)}`);
-                                } catch (err) {
-                                    alert(`添加资源失败! message: ${err}`);
-                                    window.utools.outPlugin();
-                                }
-                            })
-                            .catch(err => {
-                                alert(`上传失败! error: ${err}`);
-                                window.utools.outPlugin();
-                            });
-                    } else {
-                        alert("内部错误!");
-                        window.utools.outPlugin();
-                    }
-                } else if (type === "files") {
-                    let resourceIdList = utools.dbStorage.getItem("memos_resource_id_list");
-                    if (!resourceIdList) {
-                        resourceIdList = [];
-                    }
-                    const openid = utools.dbStorage.getItem("memos_openid");
-                    const url = utools.dbStorage.getItem("memos_url");
-                    if (openid && url) {
-                        const fetches = payload.filter((file) => file.isFile).map((file) => {
-                            const form = new FormData();
-                            const data = fs.readFileSync(file.path);
-                            form.append('file', new Blob([data]), file.name);
-                            return fetch(`${url}/api/resource?openId=${openid}`, {
-                                method: 'POST',
-                                body: form,
-                            });
-                        });
-                        Promise.all(fetches)
-                            .then(responses => {
-                                return Promise.all(responses.map(function (response) {
-                                    return response.json();
-                                }));
-                            })
-                            .then(data => {
-                                Promise.all(data.map(function (dt) {
-                                    if (dt.data) {
-                                        resourceIdList.push(dt.data.id)
-                                    } else {
-                                        alert(`上传失败! error: ${JSON.stringify(dt)}`);
-                                        window.utools.outPlugin();
-                                    }
-                                }))
-                                    .then(() => {
-                                        try {
-                                            utools.dbStorage.setItem("memos_resource_id_list", resourceIdList);
-                                            alert(`添加资源成功! resourceIdList: ${JSON.stringify(resourceIdList)}`);
-                                            window.utools.outPlugin();
-                                        } catch (err) {
-                                            alert(`添加资源失败! message: ${err}`);
-                                            window.utools.outPlugin();
-                                        }
-                                    });
-
-                            })
-                            .catch(err => {
-                                alert(`上传失败! error: ${err}`);
-                                window.utools.outPlugin();
-                            });
-
-                    } else {
-                        alert("内部错误!");
-                        window.utools.outPlugin();
-                    }
-                } else {
-                    alert("内部错误!");
-                    window.utools.outPlugin();
-                }
+                appendContent(action, true, window.utools.outPlugin, window.utools.outPlugin);
             }
         }
     },
@@ -184,8 +150,7 @@ window.exports = {
             enter: () => {
                 window.utools.hideMainWindow()
                 try {
-                    utools.dbStorage.removeItem("memos_content");
-                    utools.dbStorage.removeItem("memos_resource_id_list");
+                    clearContent();
                     alert("清空暂存区成功!");
                 } catch (err) {
                     alert(`清空暂存区失败! message: ${err}`);
@@ -194,22 +159,39 @@ window.exports = {
             }
         }
     },
-    "openapi": {
+    "token": {
         mode: "none",
         args: {
             enter: (action) => {
                 window.utools.hideMainWindow()
-                const openapi = action.payload;
+                const token = action.payload;
                 try {
-                    const [url, openid] = openapi.split("/api/memo?openId=");
-                    utools.dbStorage.setItem("memos_openid", openid);
-                    utools.dbStorage.setItem("memos_url", url);
-                    alert(`设置 OpenAPI 成功! (${openapi})`);
+                    utools.dbStorage.setItem("memos_access_token", token);
+                    alert(`设置 Access Token 成功! (${token})`);
                 } catch (err) {
-                    alert(`设置 OpenAPI 失败! message: ${err}`);
+                    alert(`设置 Access Token 失败! message: ${err}`);
                 }
                 window.utools.outPlugin()
             },
         }
-    }
+    },
+    "url": {
+        mode: "none",
+        args: {
+            enter: (action) => {
+                window.utools.hideMainWindow()
+                let url = action.payload;
+                if (url.endsWith("/")) {
+                    url = url.slice(0, -1);
+                }
+                try {
+                    utools.dbStorage.setItem("memos_url", url);
+                    alert(`设置 URL 成功! (${url})`);
+                } catch (err) {
+                    alert(`设置 URL 失败! message: ${err}`);
+                }
+                window.utools.outPlugin()
+            },
+        }
+    },
 }
